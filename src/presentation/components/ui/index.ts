ee7c8bce8d931dac568a9556fbdd7e3a4ff761bc
sadamhusein77.sh <sadamhusein77.sh@gmail.com>
2026-05-11 07:40:54 +0700

    feat: [CODE-0000] [SADAM] Add some shared component

--- src/presentation/components/ui/index.ts
+++ src/presentation/components/ui/index.ts
@@ -1,2 +1,25 @@
-export { Button } from './Button';
-export { Select } from './Select';
+export { Button, buttonVariants } from './button';
+export { Input } from './input';
+export { Label } from './label';
+export { Textarea } from './textarea';
+export { Checkbox } from './checkbox';
+export {
+  Select,
+  SelectGroup,
+  SelectValue,
+  SelectTrigger,
+  SelectContent,
+  SelectItem,
+} from './select';
+export {
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+} from './dialog';
